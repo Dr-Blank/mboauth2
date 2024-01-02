ac2c1740549d11ab55ad96c0f4d9a98af02f3f85
Dr-Blank <64108942+Dr-Blank@users.noreply.github.com>
2024-01-02 09:07:53 -0500

    ✨ Hello World

---
+++ tests/test_url_generation.py
@@ -0,0 +1,16 @@
+from mboauth2 import AuthClient, Scope
+from mboauth2.app import OAUTH_ENDPOINT
+
+client = AuthClient(
+    client_id="client_id",
+    client_secret="client_secret",
+    redirect_uri="https://example.com",
+)
+
+
+def test_generate_authorization_url():
+    authorization_url = client.generate_auth_url(
+        scopes=[Scope.PROFILE, Scope.EMAIL],
+        state="state",
+    )
+    assert authorization_url.startswith(OAUTH_ENDPOINT)
